fc0d762cf7ebecd3ec8a8bf2d658a8db64ddf5b4
Nicolas vauché <hello@nicolasvauche.net>
2022-11-08 13:31:10 +0100

    Initial commit

---
+++ connexion.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>Connexion - Authentification avec PHP</title>
+
+        <link rel="stylesheet" href="assets/css/styles.min.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <a href="./">Accueil</a>
+                <a href="./connexion.php" class="active">Connexion</a>
+                <a href="./deconnexion.php" onclick="return window.confirm('Êtes-vous sûr(e) ?')">Déconnexion</a>
+            </nav>
+        </header>
+
+        <main>
+            <section>
+                <h1>Connexion</h1>
+                <form action="src/php/forms/form-login.php" method="post">
+                    <div>
+                        <label for="userEmail" class="required">Adresse e-mail</label>
+                        <input type="email" name="email" id="userEmail" required />
+                        <p class="error">Adresse e-mail invalide</p>
+                    </div>
+                    <div>
+                        <label for="userPassword" class="required">Mot de passe</label>
+                        <input type="password" name="password" id="userPassword" required />
+                        <p class="error">Mot de passe invalide</p>
+                    </div>
+                    <div>
+                        <button type="submit">Je me connecte &raquo;</button>
+                    </div>
+                </form>
+            </section>
+        </main>
+
+        <footer>
+            <p class="copyrights">©2022 ALIPTIC - Design: Nicolas Vauché</p>
+        </footer>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,32 @@
+<!doctype html>
+<html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+        <title>Accueil - Authentification avec PHP</title>
+
+        <link rel="stylesheet" href="assets/css/styles.min.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <a href="./" class="active">Accueil</a>
+                <a href="./connexion.php">Connexion</a>
+                <a href="./deconnexion.php" onclick="return window.confirm('Êtes-vous sûr(e) ?')">Déconnexion</a>
+            </nav>
+        </header>
+
+        <main>
+            <section>
+                <h1>Accueil</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam consectetur cupiditate debitis dolore eaque eos illum ipsa iste labore, natus nisi odio omnis perspiciatis quas repellat repudiandae tenetur unde voluptatum.</p>
+            </section>
+        </main>
+
+        <footer>
+            <p class="copyrights">©2022 ALIPTIC - Design: Nicolas Vauché</p>
+        </footer>
+    </body>
+</html>
